4678092e53bb09119fa6ee88ee9df753a0cb938f
xm-71 <alexcannon2@live.com>
2016-06-24 10:55:17 -0400

    navbar moved and working

---
+++ app/layout/navbar.directive.js
@@ -0,0 +1,13 @@
+(function(){
+'use strict';
+
+angular.module('mutantApp.layout').directive('xtNavbar', xtNavbar);
+
+function xtNavbar(){
+  return {
+    templateUrl: 'app/layout/navbar.html',
+    restrict: 'E',
+  }
+}
+
+})();
